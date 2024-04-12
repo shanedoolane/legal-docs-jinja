a1489797e2c264cb07973b6279f6d1941efe95a8
shanedoolane <58960342+shanedoolane@users.noreply.github.com>
2024-04-11 17:05:59 -0700

    generate the HTML output

--- employee-nda/generate.py
+++ employee-nda/generate.py
@@ -1,1 +1,18 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+import markdown
+import yaml
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('employee-nda.md')
+
+# Load data from the YAML file
+with open('info.yml', 'r') as yaml_file:
+    data = yaml.safe_load(yaml_file)
+
+# Render the template with the data
+rendered_content = template.render(data)
+
+# Convert the rendered content from Markdown to HTML
+html_content = markdown.markdown(rendered_content)
+
+print(html_content)
